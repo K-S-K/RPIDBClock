1fad13a462201b953f3e4fc5fd70259da6bf8138
KSK <st.kiselevskii@proton.me>
2024-08-18 17:35:26 +0200

    RDC-5: Configure-Deployment-to-RPI

---
+++ Src/deploy-rpi-direct.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Variables
+RPI_USER="ksk"
+RPI_IP="192.168.1.106"
+RPI_PATH="~/Projects/DBClock"
+LOCAL_PATH="/Users/ksk-work/Projects/RPI/RPIDBClock/Src"
+
+# Sync local project files to RPI
+rsync -vv -avz --exclude 'bin/' --exclude 'obj/' $LOCAL_PATH $RPI_USER@$RPI_IP:$RPI_PATH
+
+# SSH into RPI and build & run the project
+ssh -t $RPI_USER@$RPI_IP << EOF
+  cd $RPI_PATH
+  cd Src
+  dotnet build
+  dotnet run --project rdc-svc/rdc-svc.csproj
+EOF
